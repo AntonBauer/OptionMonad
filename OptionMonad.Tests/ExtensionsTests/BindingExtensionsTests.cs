e1c86a9ada1ed8338f45bff6e394369d29c794ef
BauerAV <abton.bauer@gmail.com>
2021-03-30 13:16:29 +0200

    Add bind tests

---
+++ OptionMonad.Tests/ExtensionsTests/BindingExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OptionMonad.ValueOptionExtensions;
+using System;
+
+namespace OptionMonad.Tests.ExtensionsTests
+{
+    [Category("OptionExtensions")]
+    public class BindingExtensionsTests
+    {
+        [Test]
+        public void BindToSomeTest()
+        {
+            // Arrange
+            var option = "Hello there".Some<string, int>();
+            Func<string, int> @delegate = val => val.Length;
+
+            // Act
+            var actual = option.Bind(@delegate);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual, Is.Not.Null);
+                Assert.That(actual, Is.InstanceOf<Option<int, int>>());
+                Assert.That(actual.IsSome(), Is.True);
+                Assert.That((actual as SomeOption<int, int>).Value, Is.EqualTo(11));
+            });
+        }
+    }
+}
