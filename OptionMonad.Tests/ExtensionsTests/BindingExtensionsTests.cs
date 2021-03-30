5ef1416b6dbfba61fb1eebf6473dd8c5e06312b9
BauerAV <abton.bauer@gmail.com>
2021-03-30 13:25:29 +0200

    Add bind extension tests

--- OptionMonad.Tests/ExtensionsTests/BindingExtensionsTests.cs
+++ OptionMonad.Tests/ExtensionsTests/BindingExtensionsTests.cs
@@ -8,7 +8,7 @@ namespace OptionMonad.Tests.ExtensionsTests
     public class BindingExtensionsTests
     {
         [Test]
-        public void BindToSomeTest()
+        public void BindSomeToPlainTest()
         {
             // Arrange
             var option = "Hello there".Some<string, int>();
@@ -24,6 +24,110 @@ namespace OptionMonad.Tests.ExtensionsTests
                 Assert.That(actual, Is.InstanceOf<Option<int, int>>());
                 Assert.That(actual.IsSome(), Is.True);
                 Assert.That((actual as SomeOption<int, int>).Value, Is.EqualTo(11));
+            });
+        }
+
+        [Test]
+        public void BindNoneToPlainTest()
+        {
+            // Arrange
+            var error = 42;
+            var option = error.None<string, int>();
+            Func<string, int> @delegate = val => val.Length;
+
+            // Act
+            var actual = option.Bind(@delegate);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual, Is.Not.Null);
+                Assert.That(actual, Is.InstanceOf<Option<int, int>>());
+                Assert.That(actual.IsNone(), Is.True);
+                Assert.That((actual as NoneOption<int, int>).Error, Is.EqualTo(error));
+            });
+        }
+
+        [Test]
+        public void BindSomeToSomeOptionTest()
+        {
+            // Arrange
+            var option = "Hello there".Some<string, int>();
+            Func<string, Option<int, int>> @delegate = val => val.Length.Some<int, int>();
+
+            // Act
+            var actual = option.Bind(@delegate);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual, Is.Not.Null);
+                Assert.That(actual, Is.InstanceOf<Option<int, int>>());
+                Assert.That(actual.IsSome(), Is.True);
+                Assert.That((actual as SomeOption<int, int>).Value, Is.EqualTo(11));
+            });
+        }
+
+        [Test]
+        public void BindSomeToNoneOptionTest()
+        {
+            // Arrange
+            var option = "Hello there".Some<string, int>();
+            Func<string, Option<int, int>> @delegate = val => 42.None<int, int>();
+
+            // Act
+            var actual = option.Bind(@delegate);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual, Is.Not.Null);
+                Assert.That(actual, Is.InstanceOf<Option<int, int>>());
+                Assert.That(actual.IsNone(), Is.True);
+                Assert.That((actual as NoneOption<int, int>).Error, Is.EqualTo(42));
+            });
+        }
+
+        [Test]
+        public void BindNoneToSomeOptionTest()
+        {
+            // Arrange
+            var error = 42;
+            var option = error.None<string, int>();
+            Func<string, Option<int, int>> @delegate = val => val.Length.Some<int, int>();
+
+            // Act
+            var actual = option.Bind(@delegate);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual, Is.Not.Null);
+                Assert.That(actual, Is.InstanceOf<Option<int, int>>());
+                Assert.That(actual.IsNone(), Is.True);
+                Assert.That((actual as NoneOption<int, int>).Error, Is.EqualTo(error));
+            });
+        }
+
+        [Test]
+        public void BindNoneToNoneOptionTest()
+        {
+            // Arrange
+            var error = 42;
+            var internalError = 24;
+            var option = error.None<string, int>();
+            Func<string, Option<int, int>> @delegate = val => internalError.None<int, int>();
+
+            // Act
+            var actual = option.Bind(@delegate);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual, Is.Not.Null);
+                Assert.That(actual, Is.InstanceOf<Option<int, int>>());
+                Assert.That(actual.IsNone(), Is.True);
+                Assert.That((actual as NoneOption<int, int>).Error, Is.EqualTo(error));
             });
         }
     }
