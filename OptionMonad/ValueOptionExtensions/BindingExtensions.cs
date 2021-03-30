aa13492f5883d4d8228a265ef611a1675c570b9c
BauerAV <abton.bauer@gmail.com>
2021-03-30 12:25:43 +0200

    Rearranged extensions

---
+++ OptionMonad/ValueOptionExtensions/BindingExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OptionMonad.ValueOptionExtensions
+{
+    public static class BindingExtensions
+    {
+        public static Option<TNextValue, TError> Bind<TValue, TNextValue, TError>(this Option<TValue, TError> option, Func<TValue, Option<TNextValue, TError>> next) =>
+            option switch
+            {
+                SomeOption<TValue, TError> some => next(some.Value),
+                NoneOption<TValue, TError> none => Option<TNextValue, TError>.None(none.Error),
+                _ => Option<TNextValue, TError>.None(),
+            };
+    }
+}
---
+++ OptionMonad/ValueOptionExtensions/GeneralExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace OptionMonad.ValueOptionExtensions
+{
+    public static class GeneralExtensions
+    {
+        public static Option<TValue, TError> Some<TValue, TError>(this TValue value) => Option<TValue, TError>.Some(value);
+
+        public static Option<TValue, TError> None<TValue, TError>(this TError error) => Option<TValue, TError>.None(error);
+    }
+}
