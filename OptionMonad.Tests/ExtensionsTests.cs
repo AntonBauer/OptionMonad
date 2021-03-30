39865152adc12193c3dad703209ac40bee72302b
BauerAV <abton.bauer@gmail.com>
2021-03-30 12:20:22 +0200

    Add option creation test

---
+++ OptionMonad.Tests/ExtensionsTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace OptionMonad.Tests
+{
+    [Category("OptionExtensions")]
+    public class ExtensionsTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
