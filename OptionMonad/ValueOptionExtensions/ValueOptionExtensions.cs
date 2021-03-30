cc2a7c9fe8cf38fd0791d28582d68da813f1c3d9
BauerAV <abton.bauer@gmail.com>
2021-03-30 11:41:01 +0200

    Add helper extensions and Bind function

---
+++ OptionMonad/ValueOptionExtensions/ValueOptionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OptionMonad.ValueOptionExtensions
+{
+    public static class ValueOptionExtensions
+    {
+        public static bool IsSome<TValue, TError>(this Option<TValue, TError> option) => option is SomeOption<TValue, TError>;
+
+        public static bool IsNone<TValue, TError>(this Option<TValue, TError> option) => option is NoneOption<TValue, TError>;
+
+        public static Option<TNextValue, TError> Bind<TValue, TNextValue, TError>(this Option<TValue, TError> option, Func<TValue, Option<TNextValue, TError>> next) =>
+            option switch
+            {
+                SomeOption<TValue, TError> some => next(some.Value),
+                NoneOption<TValue, TError> none => Option<TNextValue, TError>.None(none.Error),
+                _ => Option<TNextValue, TError>.None(),
+            };
+    }
+}
